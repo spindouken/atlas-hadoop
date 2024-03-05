97871f676aad1aa6f1ac4f9f074c016d10a1cf42
spindouken <mlc9181209@gmail.com>
2024-03-05 17:58:18 -0600

    hadoop_project Docker buildout

---
+++ hadoop-project/6-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.7
+
+download = __import__('6-download').download
+
+
+l = ["/holbies/input/lao.txt"]
+download(l)
+lao =  open("/tmp/lao.txt", "r")
+print(lao.read())
+lao.close()
